a8e75b84e5c71f9f2c00653eb8afcc73dff27eb3
David J. Malan <malan@harvard.edu>
2019-02-21 23:03:00 -0500

    adds lua link

--- cs50.sh
+++ cs50.sh
@@ -72,6 +72,11 @@ wget -P /tmp https://download.java.net/java/GA/jdk10/10.0.1/fb4372174a714e6b8c52
     ln -s /opt/jdk-10.0.1/bin/* /opt/bin/ && \
     chmod a+rx /opt/bin/*
 
+# Lua-specific
+# https://askubuntu.com/a/1035151
+update-alternatives --install /usr/bin/lua lua-interpreter /usr/bin/lua5.3 130 --slave /usr/share/man/man1/lua.1.gz lua-manual /usr/share/man/man1/lua5.3.1.gz
+update-alternatives --install /usr/bin/luac lua-compiler /usr/bin/luac5.3 130 --slave /usr/share/man/man1/luac.1.gz lua-compiler-manual /usr/share/man/man1/luac5.3.1.gz
+
 # LÖVE-specific
 # https://bitbucket.org/rude/love/downloads/
 wget -P /tmp https://bitbucket.org/rude/love/downloads/love_0.10.2ppa1_amd64.deb && \
