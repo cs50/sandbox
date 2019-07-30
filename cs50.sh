e535865b8e9d08d3a25aa461de8873a8fb2765a7
David J. Malan <malan@harvard.edu>
2019-07-30 19:29:15 -0400

    added Swift

--- cs50.sh
+++ cs50.sh
@@ -122,6 +122,12 @@ apt-get update && \
     rm -rf Python-3.7.3 && \
     pip3 install --upgrade pip
 
+# Install Swift 5.0
+RUN cd /tmp && \
+    wget https://swift.org/builds/swift-5.0.2-release/ubuntu1804/swift-5.0.2-RELEASE/swift-5.0.2-RELEASE-ubuntu18.04.tar.gz && \
+    tar xzf swift-5.0.2-RELEASE-ubuntu18.04.tar.gz --strip-components=1 -C / && \
+    rm -f swift-5.0.2-RELEASE-ubuntu18.04.tar.gz
+
 # Install CS50 packages
 pip3 install \
         cs50 \
@@ -230,6 +236,7 @@ if [ "$PS1" ]; then
     # Environment variables
     export CC="clang"
     export CFLAGS="-fsanitize=signed-integer-overflow -fsanitize=undefined -ggdb3 -O0 -std=c11 -Wall -Werror -Wextra -Wno-sign-compare -Wno-unused-parameter -Wno-unused-variable -Wshadow"
+    export CLASSPATH ".:/usr/share/java/cs50.jar"
     export EDITOR=nano
     export FLASK_APP=application.py
     export FLASK_ENV=development
